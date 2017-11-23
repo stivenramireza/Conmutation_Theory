c7ccc44f3734c78b3179e29b5857ce27b2b986a0
Stiven Ramírez Arango <you@example.com>
2017-11-23 12:01:51 -0500

    3

--- invernadero.py
+++ invernadero.py
@@ -80,7 +80,7 @@ def main():
         c = arduino.read()
         print c
         if not c:
-            cont = urllib2.urlopen("%s/commands/execute?api_key=%s"%(talkback_url, talkback_key))
+            cont = urllib2.urlopen("https://api.thingspeak.com/talkbacks/20308/commands?api_key=KLT3FI87ARIIAXV4")
             command = cont.read()
             if command == 'manual':
                 arduino.write('M')
